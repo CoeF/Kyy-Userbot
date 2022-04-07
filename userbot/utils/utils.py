bd3f9f9489923067dbc32ed6f0c731ca3d24b439
X Not For U <57592261+CoeF@users.noreply.github.com>
2022-04-08 01:43:03 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -313,7 +313,7 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
 def git():
     UPSTREAM_REPO = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL211aGFtbWFkcml6a3kxNi9LeXktVXNlcmJvdA=="
+        "aHR0cHM6Ly9naXRodWIuY29tL0NvZUYvS3l5LVVzZXJib3Q="
     ).decode("utf-8")
     try:
         repo = Repo()
